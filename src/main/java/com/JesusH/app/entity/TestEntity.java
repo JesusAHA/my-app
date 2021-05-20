793a6d8cc7aef4f4e1ee8fb9155983deb3ae4431
Jesus Antonio Hernandez Alvarez <tonyhunter696969@gmail.com>
2021-05-19 20:44:58 -0500

    Nombre del proyecto: my-app. Version final del my-add

---
+++ src/main/java/com/JesusH/app/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.JesusH.app.entity;
+
+/**
+ * @author Jesus_H
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity.
+	 */
+	
+     private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
